2b5b0a55d22bf1b6769b28e4bb890e4dc26bff55
Abramov21 <104825295+Abramov21@users.noreply.github.com>
2023-05-27 23:38:23 +0300

    fixed

--- middlewares/validateBody.js
+++ middlewares/validateBody.js
@@ -2,7 +2,6 @@ const { HttpError } = require("../helpers/index");
 
 const validateBody = (schema) => {
   const func = async (req, res, next) => {
-    if (!Object.keys(req.body).length) next(HttpError(400, "missing fields"));
     const { error } = schema.validate(req.body);
     console.log(schema.validate(req.body).error);
     if (error) {
--- models/contacts.js
+++ models/contacts.js
@@ -17,26 +17,31 @@ const schemaAddContact = Joi.object({
 
 const updateFavoriteSchema = Joi.object({
   favorite: Joi.boolean().required().messages({
-    "any.required": `missing required favorite field`,
+    "any.required": `missing field favorite`,
   }),
 });
 
-const contactSchema = new Schema({
-  name: {
-    type: String,
-    required: [true, "Set name for contact"],
+const contactSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Set name for contact"],
+    },
+    email: {
+      type: String,
+    },
+    phone: {
+      type: String,
+    },
+    favorite: {
+      type: Boolean,
+      default: false,
+    },
   },
-  email: {
-    type: String,
-  },
-  phone: {
-    type: String,
-  },
-  favorite: {
-    type: Boolean,
-    default: false,
-  },
-});
+  {
+    versionKey: false,
+  }
+);
 
 contactSchema.post("save", handleMongooseError);
 
